a1ce9a31ae053a1313090a72d3c6dd4948fec51f
maxwell.maier <maxwellmaier89@gmail.com>
2024-03-11 12:09:43 -0500

    part 1 completed

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books Read";
+   let book1 = "https://dw9to29mmj727.cloudfront.net/products/1974743292.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/419AtPPHdJL._SX342_SY445_.jpg";
+   let book3 = "https://upload.wikimedia.org/wikipedia/commons/b/ba/ArthurConanDoyle_AStudyInScarlet.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Spy X Family Vol. 11" />
+         <img src={book2} alt="Think Like a Detective" />
+         <img src={book3} alt="Sherlock Holmes A Study in Scarlet" />
       </div>      
    );
 }
